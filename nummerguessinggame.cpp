eb5bd15dccf54e6910106e3d053d48d9b35fd288
EsadCC <9028564@student.zadkine.nl>
2026-06-20 13:00:23 +0200

    Create nummerguessinggame.cpp

---
+++ nummerguessinggame.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+    int num;
+    int guess;
+    int tries = 0;
+
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+
+    std::cout << "***** NUMBER GUESSING GAME *****\n";
+
+    do{
+        std::cout << "Enter a guess between (1-100): ";
+        std::cin >> guess;
+        tries++;
+
+        if(guess > num)
+        {
+            std::cout << "Too high!\n";
+        }
+        else if(guess < num)
+        {
+            std::cout << "Too low!\n";
+        }
+        else
+        {
+            std::cout << "CORRECT!\n";
+            std::cout << "It took you: " << tries << " tries to guess the number!\n";
+        }
+    }while(guess != num);
+
+    return 0;
+}
